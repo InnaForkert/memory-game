8c25d8fae15ffb47ed55211512ba91c9495654d6
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-05 19:25:15 +0200

    init + pictures

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Magic Memory Game</h1>
+      <button>Start game</button>
+    </div>
+  );
+}
+
+export default App;
